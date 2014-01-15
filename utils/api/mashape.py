f664ebdc42c97aaf4641490c36a02357101ad490
Jacob Bollinger <jacob@ip-172-31-33-228.us-west-2.compute.internal>
2014-01-15 02:53:25 +0000

    add example to header

--- utils/api/mashape.py
+++ utils/api/mashape.py
@@ -13,6 +13,8 @@
 #Documentation Examples:                                                                                                                        curl --include --request GET 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-text/?text=Friends%20and%20fellow%20sailors%20mourned%20double%20Olympic%20medalist%20Andrew%20%22Bart%22%20Simpson%20after%20the%20shocking%20news%20that%20he%20had%20died%20in%20San%20Francisco%20Bay%20while%20training%20for%20the%20America's%20Cup.' --header "X-Mashape-Authorization: iX7o6d4rTKAr4lcq1T209WJRp7aFQoJn"  
 
 #curl --include --request GET 'https://loudelement-free-natural-language-processing-service.p.mashape.com/nlp-url/?url=http%3A%2F%2Fwww.cnn.com%2F2013%2F05%2F09%2Fus%2Fohio-missing-women-found%2F' --header "X-Mashape-Authorization: iX7o6d4rTKAr4lcq1T209WJRp7aFQoJn" 
+
+#response = unirest.get("https://duckduckgo-duckduckgo-zero-click-info.p.mashape.com/?q=DuckDuckGo&callback=process_duckduckgo&no_html=1&no_redirect=1&skip_disambig=1&format=json",headers={"X-Mashape-Authorization": "iX7o6d4rTKAr4lcq1T209WJRp7aFQoJn"});
 ##############################
 
 import urllib2
